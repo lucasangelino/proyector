c0fcb181c0d1b00acf2cf3b4636c57ff39a2b68f
ANGELINO <langelin@TMOVILES.COM.AR>
2020-02-12 19:18:04 -0300

    cambio de IP para heroku

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth')); 
@@ -22,6 +22,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas')); 
 
 
-app.listen(PORT, () => {
-    console.log(`Server UP in PORT: ${PORT}`)
+app.listen(port,'0.0.0.0', () => {
+    console.log(`Server UP in PORT: ${port}`)
 }); 
